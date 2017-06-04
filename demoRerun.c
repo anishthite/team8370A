762e8ff73e32da89856dd169f590895a78fbcb87
Anish Thite <anishthite@live.com>
2017-06-04 10:47:54 -0400

    updats and add DemoRun

--- demoRerun.c
+++ demoRerun.c
@@ -2,39 +2,18 @@
 //records input data given by remote and prints it out- to be used in
 
 
-//initialize data arrays
-//record and run --> save joystick value to data array for
-int joystickValue;
-
 
 //initialize arrays, will record tenth of a second (not enough mem for hundreth)- compression?
 /** Customize per robot **/
 int rightDrive[150];
 int leftDrive[150];
-int motor3[150];
-int motor4[150];
-int motor5[150];
-int motor6[150];
-int motor7[150];
-int motor8[150];
-int motor9[150];
-int motor10[150];
+
 //initializes arrays to 0
 void initializeArray(int* motor){
 		for(int i=0;i<150;i++)
 	{
 		motor[i]=0;
 	}
-
-}
-void recordAndRun(){
-for (int i = 0; i < 150; i++){
-	/** Customize per robot **/
-	//set equal to input device (button or joystick)
-	rightDrive[i] = vexRT[Ch2];
-	leftDrive[i] = vexRT[Ch3];
-	wait10Msec(1);
-}
 
 }
 //initializes arrays and clears debug stream
@@ -45,6 +24,18 @@ void initialize(){
 	initializeArray(leftDrive);
 
 }
+void recordAndRun(){
+for (int i = 0; i < 150; i++){
+	/** Customize per robot **/
+	//set equal to input device (button or joystick)
+	rightDrive[i] = vexRT[Ch2];
+	leftDrive[i] = vexRT[Ch3];
+	wait10Msec(1);
+	//shuts off all motors
+}
+
+}
+
 task main()
 {
 /** paste code to run robot here**/
